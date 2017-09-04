d5817e9a883c66dabc1b28584fa365d01e31ce87
xuzg <xuzg@broada.com>
2017-09-04 20:24:08 +0800

    chapter_3

--- chapter_1/mysite/polls/views.py
+++ chapter_1/mysite/polls/views.py
@@ -7,6 +7,13 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello,world. You're at the polls index")
 
+def results(request,question_id):
+    response = "You're looking at the result  of question %s"
+    return HttpResponse(response %question_id)
+
+def vote(request,question_id):
+    return HttpResponse("You're voting on question %s" %question_id)
+
 
     
 
